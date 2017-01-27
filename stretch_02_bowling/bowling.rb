936e24d2ef117d8250f255718eb289e0a949e1ba
Daniel Moniz <daniel.moniz@gmail.com>
2017-01-26 19:32:43 -0500

    Add recursive solution.

--- stretch_02_bowling/bowling.rb
+++ stretch_02_bowling/bowling.rb
@@ -1,51 +1,16 @@
 class Game
-
-  def score(rolls)
-    score = 0
-    frames = self.get_frames(rolls)
-    bonus_rolls_remaining = []
-
-    frames.each.with_index do |frame, i|
-      score += frame.reduce(:+)
-      frame.each do |roll|
-        bonus_rolls_remaining.map! do |bonus|
-          score += roll if bonus > 0
-          next bonus - 1
-        end
-      end
-      bonus_rolls_remaining.select! { |x| x > 0 }
-
-      if frame[0] == 10 # strike
-        bonus_rolls_remaining << 2
-      elsif frame[0..1].reduce(:+) == 10 # spare
-        bonus_rolls_remaining << 1
-      end
-
+  def score(rolls, frame=1)
+    return rolls[0..-1].reduce(:+) if frame == 10
+    puts rolls.to_s
+    if rolls[0] == 10
+      # strike
+      return rolls[0..2].reduce(:+) + self.score(rolls[1..-1], frame + 1)
+    elsif rolls[0..1].reduce(:+) == 10
+      # spare
+      return rolls[0..2].reduce(:+) + self.score(rolls[2..-1], frame + 1)
+    else
+      # normal frame
+      return rolls[0..1].reduce(:+) + self.score(rolls[2..-1], frame + 1)
     end
-
-    return score
-  end
-
-  def get_frames(rolls)
-    frames = []
-    sum = 0
-    num_rolls = 0
-    frame = []
-    rolls.each do |roll|
-      frame << roll
-      sum += roll
-      num_rolls += 1
-      if frames.length == 10
-        frames[-1] << roll
-      elsif sum == 10 || (num_rolls == 2)
-        frames << frame
-        frame = []
-        sum = 0
-        num_rolls = 0
-      else
-
-      end
-    end
-    return frames
   end
 end
